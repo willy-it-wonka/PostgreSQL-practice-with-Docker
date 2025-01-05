ed2ff58ae13005f9398ace9bedfba4a1a3c51395
Willy <willy.it.wonka@outlook.com>
2025-01-05 23:33:06 +0100

    Create tables with first run of containers

---
+++ docker/init.sql
@@ -0,0 +1,20 @@
+CREATE TABLE IF NOT EXISTS employees (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    salary DECIMAL(10, 2),
+    birthday DATE,
+    hire_date DATE,
+    department VARCHAR(50),
+    position VARCHAR(100)
+);
+
+CREATE TABLE IF NOT EXISTS contacts (
+    employee_id INTEGER PRIMARY KEY,
+    email VARCHAR(100) UNIQUE,
+    phone_number VARCHAR(50),
+    address TEXT,
+    city VARCHAR(100),
+    postal_code VARCHAR(20),
+    FOREIGN KEY (employee_id) REFERENCES employees(id) ON DELETE CASCADE
+);
