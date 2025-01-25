6f8f6c1cc23dbdef693744fae161fc5de551a817
Willy <willy.it.wonka@outlook.com>
2025-01-25 22:01:44 +0100

    Refactor generate_employees.py

--- scripts/generate_employees.py
+++ scripts/generate_employees.py
@@ -4,8 +4,8 @@ import random
 from faker import Faker
 
 fake = Faker()
-departments = ['IT', 'Marketing', 'Sales', 'Finance', 'Legal', 'HR']
-department_positions = {
+DEPARTMENTS = ['IT', 'Marketing', 'Sales', 'Finance', 'Legal', 'HR']
+DEPARTMENT_POSITIONS = {
     'IT': ['Back-end Developer', 'Front-end Developer', 'Network Administrator', 'DevOps Engineer'],
     'Marketing': ['Marketing Specialist', 'Social Media Manager', 'SEO Analyst'],
     'Sales': ['Sales Executive', 'Account Manager'],
@@ -13,27 +13,38 @@ department_positions = {
     'Legal': ['Lawyer', 'Legal Advisor', 'Contract Specialist'],
     'HR': ['HR Manager', 'Recruitment Specialist', 'HR Assistant']
 }
+FIELDNAMES = ['id', 'first_name', 'last_name', 'salary', 'birthday', 'hire_date', 'department', 'position']
 
 
 def generate_employee_data(employee_id):
-    department = random.choice(departments)
+    department = random.choice(DEPARTMENTS)
     return {
         'id': employee_id,
         'first_name': fake.first_name(),
         'last_name': fake.last_name(),
-        'salary': round(random.uniform(3000, 10000), 2),
+        'salary': generate_salary(),
         'birthday': fake.date_of_birth(minimum_age=18, maximum_age=75).strftime('%Y-%m-%d'),
         'hire_date': fake.date_between(start_date='-10y', end_date='today').strftime('%Y-%m-%d'),
         'department': department,
-        'position': random.choice(department_positions[department]),
+        'position': random.choice(DEPARTMENT_POSITIONS[department]),
     }
 
 
-employees = []
-for emp_id in range(1, 501):
-    employees.append(generate_employee_data(emp_id))
+def generate_salary(min_salary=3000, max_salary=10000):
+    return round(random.uniform(min_salary, max_salary), 2)
 
-with open('employees.csv', mode='w', newline='', encoding='utf-8') as file:
-    writer = csv.DictWriter(file, fieldnames=['id', 'first_name', 'last_name', 'salary', 'birthday', 'hire_date', 'department', 'position'])
-    writer.writeheader()
-    writer.writerows(employees)
+
+def generate_employees(num_employees=500):
+    return [generate_employee_data(emp_id) for emp_id in range(1, num_employees + 1)]
+
+
+def save_employees_to_csv(employee_data, filename='employees.csv'):
+    with open(filename, mode='w', newline='', encoding='utf-8') as file:
+        writer = csv.DictWriter(file, fieldnames=FIELDNAMES)
+        writer.writeheader()
+        writer.writerows(employee_data)
+
+
+if __name__ == "__main__":
+    employees = generate_employees()
+    save_employees_to_csv(employees)
